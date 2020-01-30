e58f98de1a8d15be515f9a7ce0de8aa3dacffa2f
fsolari <fsolari90@gmail.com>
2020-01-30 00:42:55 -0300

    public_html

--- app.js
+++ app.js
@@ -4,11 +4,11 @@ const path = require('path');
 const router = express.Router();
 
 router.get('/',function(req,res){
-  res.sendFile(path.join(__dirname+'/public_html/index.html'));
+  res.sendFile(path.join(__dirname+'/index.html'));
 });
 
 router.get('/about',function(req,res){
-    res.sendFile(path.join(__dirname+'/public_html/about.html'));
+    res.sendFile(path.join(__dirname+'/about.html'));
   });
 
 //add the router
