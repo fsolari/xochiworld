985b811debc2d28487c7686f250c5d3b6c80112b
fsolari <fsolari90@gmail.com>
2020-02-07 19:39:30 -0300

    home wip

--- app.js
+++ app.js
@@ -8,11 +8,13 @@ const viewsPath = "/views";
 app.use(express.static(path.join(__dirname, 'public')));
 
 router.get('/', function (req, res) {
-  res.sendFile(path.join(__dirname + viewsPath + '/index.html'));
+  res.sendFile(path.join(__dirname + '/index.html'));
 });
 
-
-router.get('/about', function (req, res) {
+/**
+ * 
+ *
+ * router.get('/about', function (req, res) {
   res.sendFile(path.join(__dirname + viewsPath + '/about.html'));
 });
 
@@ -26,7 +28,9 @@ router.get('/contact', function (req, res) {
 
 router.get('/press', function (req, res) {
   res.sendFile(path.join(__dirname + viewsPath + '/press.html'));
-});
+}); 
+ */
+
 
 
 app.use('/', router);
